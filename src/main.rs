51ac9f98b17eba694699d2172f88728514ca68c1
imawizard <imawizard@outlook.de>
2023-08-27 22:44:21 +0200

    feat: Start qemu and debug with gdb/lldb from vscode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs;
+use std::os::unix;
+use std::path;
 use std::process::Command;
 
 fn main() {
@@ -7,6 +10,27 @@ fn main() {
     let mut cmd = Command::new("qemu-system-x86_64");
     cmd.arg("-drive")
         .arg(format!("format=raw,file={bios_path}"));
+
+    let mut args = env::args();
+    let first_arg = args.nth(1).unwrap_or_default().to_lowercase();
+
+    match first_arg.as_str() {
+        "debug" => {
+            cmd.arg("-d") // log ...
+                .arg("int") // interrupts/exceptions
+                .arg("-S") // freeze CPU at startup
+                .arg("-s"); // shorthand for -gdb tcp::1234
+
+            let out = path::Path::new("./target/debug/kernel");
+            if let Ok(true) = out.try_exists() {
+                fs::remove_file(out).unwrap();
+            }
+
+            let kernel_path = env!("KERNEL_BIN");
+            unix::fs::symlink(kernel_path, out).unwrap();
+        }
+        _ => {}
+    }
 
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
