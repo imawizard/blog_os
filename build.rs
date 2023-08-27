6f713d82af2207ba8f570d075ea9e4c2c82d5e8c
imawizard <imawizard@outlook.de>
2023-08-27 22:44:21 +0200

    feat: Start qemu and debug with gdb/lldb from vscode

--- build.rs
+++ build.rs
@@ -5,8 +5,8 @@ use bootloader::DiskImageBuilder;
 
 fn main() {
     // set by cargo for the kernel artifact dependency
-    let kernel_path = env::var("CARGO_BIN_FILE_KERNEL").unwrap();
-    let disk_builder = DiskImageBuilder::new(PathBuf::from(kernel_path));
+    let kernel_path = PathBuf::from(env::var("CARGO_BIN_FILE_KERNEL").unwrap());
+    let disk_builder = DiskImageBuilder::new(kernel_path.clone());
 
     // specify output paths
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -17,4 +17,7 @@ fn main() {
 
     // pass the disk image paths via environment variables
     println!("cargo:rustc-env=BIOS_IMAGE={}", bios_path.display());
+
+    // also pass the path to the compiled kernel
+    println!("cargo:rustc-env=KERNEL_BIN={}", kernel_path.display());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::os::unix;
 use std::process::Command;
 
 fn main() {
@@ -7,6 +11,27 @@ fn main() {
     let mut cmd = Command::new("qemu-system-x86_64");
     cmd.arg("-drive")
         .arg(format!("format=raw,file={bios_path}"));
+
+    let mut args = env::args();
+    let first_arg = args.nth(1).unwrap_or_default().to_lowercase();
+
+    match first_arg.as_str() {
+        "debug" => {
+            cmd.arg("-d") // log ...
+                .arg("int") // interrupts/exceptions
+                .arg("-S") // freeze CPU at startup
+                .arg("-s"); // shorthand for -gdb tcp::1234
+
+            let out = std::path::Path::new("./target/debug/kernel");
+            if let Ok(true) = out.try_exists() {
+                fs::remove_file(out).unwrap();
+            }
+
+            let kernel_path = env!("KERNEL_BIN");
+            unix::fs::symlink(kernel_path, out).unwrap();
+        }
+        _ => {}
+    }
 
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
