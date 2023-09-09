28904bff2d48c0661c220826909103b265a2d86c
imawizard <imawizard@outlook.de>
2023-09-09 16:13:01 +0200

    refactor: Add getchar for pauses

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -16,7 +16,10 @@ use core::ops::DerefMut;
 use kernel::acpi::{self, sdt, AcpiError};
 use kernel::nfit;
 use kernel::pmem;
+use kernel::task::keyboard::ScancodeStream;
 use kernel::vmem::{self, MappedRegions, UsableRegions};
+use pc_keyboard::{layouts, HandleControl, Keyboard, ScancodeSet1};
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 
@@ -30,7 +33,6 @@ macro_rules! p {
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     use kernel::allocator;
     use kernel::memory;
-    use x86_64::VirtAddr;
 
     logger::init(boot_info.framebuffer.as_mut().expect("no framebuffer"));
     log::set_max_level(log::LevelFilter::Trace);
@@ -105,51 +107,14 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     );
     vmem::MANAGER.lock().set(page_allocator).unwrap();
 
-    p!("============================");
-    p!("NFIT System Descriptor Table");
-    p!("============================");
-
-    let acpi_tables = acpi::get_tables(
-        boot_info.rsdp_addr.into_option().expect("no rsdp set"),
-        phys_mem_offset,
-    );
-
-    let nfit = unsafe {
-        acpi_tables
-            .get_sdt::<nfit::Nfit>(sdt::Signature::NFIT)
-            .unwrap()
-            .ok_or(AcpiError::TableMissing(sdt::Signature::NFIT))
-            .unwrap()
-    };
-
-    for (i, e) in nfit.entries().enumerate() {
-        use nfit::NfitEntry as E;
-        match e {
-            E::SpaRange(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::NvdimmRegionMapping(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::Interleave(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::SmbiosManagementInfo(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::NvdimmControlRegion(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::NvdimmBlockDataWindowRegion(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::FlushHintAddress(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-            E::PlatformCapabilities(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
-        }
-    }
-
-    p!("==============");
-    p!("Mapped NVDIMMs");
-    p!("==============");
-
-    unsafe {
-        pmem::MANAGER.lock().init(&nfit);
-    }
-
     #[cfg(test)]
     test_main();
 
     let mut executor = Executor::new();
-    executor.spawn(Task::new(example_task()));
-    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.spawn(Task::new(pmem_stuff(
+        boot_info.rsdp_addr.into_option(),
+        phys_mem_offset,
+    )));
     executor.run();
 }
 
@@ -173,10 +138,63 @@ fn panic(info: &PanicInfo) -> ! {
     kernel::test_panic_handler(info)
 }
 
+async fn pmem_stuff(rsdp: Option<u64>, phys_mem_offset: VirtAddr) {
+    let mut scancodes = ScancodeStream::new();
+    let mut keyboard = Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore);
+
+    keyboard::getchar(&mut scancodes, &mut keyboard).await;
+
+    p!("============================");
+    p!("NFIT System Descriptor Table");
+    p!("============================");
+
+    let acpi_tables = acpi::get_tables(rsdp.expect("no rsdp set"), phys_mem_offset);
+
+    let nfit = unsafe {
+        acpi_tables
+            .get_sdt::<nfit::Nfit>(sdt::Signature::NFIT)
+            .unwrap()
+            .ok_or(AcpiError::TableMissing(sdt::Signature::NFIT))
+            .unwrap()
+    };
+
+    for (i, e) in nfit.entries().enumerate() {
+        use nfit::NfitEntry as E;
+        match e {
+            E::SpaRange(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::NvdimmRegionMapping(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::Interleave(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::SmbiosManagementInfo(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::NvdimmControlRegion(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::NvdimmBlockDataWindowRegion(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::FlushHintAddress(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+            E::PlatformCapabilities(e) => p!("{}. NFIT Entry: {:#?}", i + 1, e),
+        }
+        keyboard::getchar(&mut scancodes, &mut keyboard).await;
+    }
+
+    p!("==============");
+    p!("Mapped NVDIMMs");
+    p!("==============");
+
+    unsafe {
+        pmem::MANAGER.lock().init(&nfit);
+    }
+
+    #[cfg(test)]
+    test_main();
+
+    keyboard::getchar(&mut scancodes, &mut keyboard).await;
+
+    println!("Done.");
+}
+
+#[allow(dead_code)]
 async fn async_number() -> u32 {
     42
 }
 
+#[allow(dead_code)]
 async fn example_task() {
     let number = async_number().await;
     println!("async number: {}", number);
--- kernel/src/task/keyboard.rs
+++ kernel/src/task/keyboard.rs
@@ -70,14 +70,30 @@ pub async fn print_keypresses() {
     let mut scancodes = ScancodeStream::new();
     let mut keyboard = Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore);
 
+    while let Some(key) = getchar(&mut scancodes, &mut keyboard).await {
+        match key {
+            DecodedKey::Unicode(character) => print!("{}", character),
+            DecodedKey::RawKey(key) => print!("{:?}", key),
+        }
+    }
+}
+
+use pc_keyboard::{KeyboardLayout, ScancodeSet};
+
+pub async fn getchar<T, S>(
+    scancodes: &mut ScancodeStream,
+    keyboard: &mut Keyboard<T, S>,
+) -> Option<DecodedKey>
+where
+    T: KeyboardLayout,
+    S: ScancodeSet,
+{
     while let Some(scancode) = scancodes.next().await {
         if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
             if let Some(key) = keyboard.process_keyevent(key_event) {
-                match key {
-                    DecodedKey::Unicode(character) => print!("{}", character),
-                    DecodedKey::RawKey(key) => print!("{:?}", key),
-                }
+                return Some(key);
             }
         }
     }
+    None
 }
