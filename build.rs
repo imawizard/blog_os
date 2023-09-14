7abab2aff34cf3b9403fb7285832369a0ff3297e
imawizard <imawizard@outlook.de>
2023-09-14 10:44:19 +0200

    chore: Symlink the resulting disk image into the target dir

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 use std::env;
+use std::fs;
+use std::os::unix;
 use std::path::PathBuf;
 
 use bootloader::DiskImageBuilder;
+
+const IMAGE_NAME: &str = "blog_os-bios.img";
 
 fn main() {
     // set by cargo for the kernel artifact dependency
@@ -10,10 +14,17 @@ fn main() {
 
     // specify output paths
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let bios_path = out_dir.join("blog_os-bios.img");
+    let bios_path = out_dir.join(IMAGE_NAME);
 
     // create the disk images
     disk_builder.create_bios_image(&bios_path).unwrap();
+
+    // symlink the disk image
+    let out = PathBuf::from("./target/debug/").join(IMAGE_NAME);
+    if let Ok(true) = out.try_exists() {
+        fs::remove_file(&out).unwrap();
+    }
+    unix::fs::symlink(&bios_path, &out).unwrap();
 
     // pass the disk image paths via environment variables
     println!("cargo:rustc-env=BIOS_IMAGE={}", bios_path.display());
